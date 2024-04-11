421cb1924fbf47d2a69da4b45ab9722362cd40ea
JeffCEmploynet <jcampbell@employnet.com>
2024-04-11 12:04:54 -0700

    Setting up login auth

---
+++ WorksBetterAPI/Models/LoginAudit.cs
@@ -0,0 +1,16 @@
+﻿namespace WorksBetterAPI.Models;
+
+public class LoginAudit
+{
+    public string UserName { get; set; }
+    public DateTime LoginDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public Guid Token { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string BranchName { get; set; }
+    public Int32 BranchId { get; set; }
+    public string UserType { get; set; }
+    public Int32 AuthLevel { get; set; }
+}
+ = 
--- WorksBetterAPI/Models/Users.cs
+++ WorksBetterAPI/Models/Users.cs
@@ -1,21 +1,16 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.SqlTypes;
-
-namespace WorksBetterAPI.Models;
+﻿namespace WorksBetterAPI.Models;
 
 public class Users
 {
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string BranchName { get; set; }
-    public Int32 BranchId { get; set; }
-    public Int32 AuthLevel { get; set; }
-    public string Email { get; set; }
+    public string UserName { get; set; }
     public string Password { get; set; }
+    public static string FirstName { get; set; }
+    public static string LastName { get; set; }
+    public static string BranchName { get; set; }
+    public static Int32 BranchId { get; set; }
+    public static string UserType { get; set; }
+    public static Int32 AuthLevel { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
 }
